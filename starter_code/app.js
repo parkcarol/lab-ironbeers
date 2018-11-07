26ba8b7deb54c5707e616399a99d13eefc813544
Caroline Park <carolpark09@gmail.com>
2018-11-07 17:36:30 -0200

    First commit

--- starter_code/app.js
+++ starter_code/app.js
@@ -10,12 +10,25 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+    punkAPI.getRandom()
+    .then(beers => {
+      res.render('beers', { beers });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
 
+
+app.get('/random-beers', (req, res, next) => {
+  res.render('randomBeers');
+});
 
 app.listen(3000);
